31e8672aba57262e5e8aa69c83d1d2e80db45f34
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-02 14:49:44 +0800

    3.17 phonebook

--- index.js
+++ index.js
@@ -112,7 +112,20 @@ app.post('/api/persons', (req, res) => {
   })
 })
 
+app.put('/api/persons/:id', (req, res) => {
+  const body = req.body
 
+  const person = {
+    name: body.name,
+    number: body.name,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
 app.delete('/api/persons/:id', (req, res) => {
   Person.findByIdAndRemove(req.params.id)
